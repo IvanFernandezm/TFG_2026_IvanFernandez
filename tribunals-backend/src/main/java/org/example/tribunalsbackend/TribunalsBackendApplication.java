73893444c402272583f13e42598d0b6e4906a3ea
IvanFernandezm <ifernandezm@edu.tecnocampus.cat>
2026-01-28 01:26:16 +0100

    Initialize Spring Boot backend project
    Creació del projecte backend amb Maven, Java 17 i amb les dependències per a Spring Data JPA, Seguretat, WebMVC i conexió a base de dades PostgreSQL.

---
+++ tribunals-backend/src/main/java/org/example/tribunalsbackend/TribunalsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tribunalsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribunalsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TribunalsBackendApplication.class, args);
+	}
+
+}
---
+++ tribunals-backend/src/test/java/org/example/tribunalsbackend/TribunalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tribunalsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribunalsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
